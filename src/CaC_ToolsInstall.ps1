524c3347dd7fe2daa5b54add66013ea65ab12425
Thomas Nieto <tnieto88@gmail.com>
2018-05-07 20:03:05 -0500

    Remove Extra Logic

--- src/CaC_ToolsInstall.ps1
+++ src/CaC_ToolsInstall.ps1
@@ -1,63 +1,11 @@
 ﻿if (Get-Command -Name choco -ErrorAction SilentlyContinue) {
-    $InstalledPackages = choco list --local-only
-    
-    if ($InstalledPackages -like '*chocolateygui*') {
-        Write-Host 'Chocolatey GUI already installed.'
-    }
-    else {
-        Write-Host 'Installing Chocolatey GUI.'
-        choco install chocolateygui -y
-    }
-    
-    if ($InstalledPackages -like '*GoogleChrome*') {
-        Write-Host 'Google Chrome already installed.'
-    }
-    else {
-        Write-Host 'Installing Google Chrome.'
-        choco install googlechrome -y
-    }
-    
-    if ($InstalledPackages -like '*nodejs*') {
-        Write-Host 'Node.Js already installed.'
-    }
-    else {
-        Write-Host 'Installing Node.Js.'
-        choco install nodejs -y
-    }
-    
-    if ($InstalledPackages -like '*git*') {
-        Write-Host 'Git already installed.'
-    }
-    else {
-        Write-Host 'Installing Git.'
-        choco install git -y
-    }
-    
-    if ($InstalledPackages -like '*atom*') {
-        Write-Host 'Atom already installed.'
-    }
-    else {
-        Write-Host 'Installing Atom.'
-        choco install atom -y
-    }
-    
-    if ($InstalledPackages -like '*gitkraken*') {
-        Write-Host 'Git Kraken already installed.'
-    }
-    else {
-        Write-Host 'Installing Git Kraken.'
-        choco install gitkraken -y
-    }
-    
-    if ($InstalledPackages -like '*Cmder*') {
-        Write-Host 'Cmder already installed.'
-    }
-    else {
-        Write-Host 'Installing Cmder.'
-        choco install cmder -y
-    }
-    
-    Write-Host 'Updating npm.'
+    choco install chocolateygui -y
+    choco install googlechrome -y
+    choco install nodejs -y
+    choco install git -y
+    choco install atom -y
+    choco install gitkraken -y
+    choco install cmder -y
     & "$($env:ProgramFiles)\nodejs\npm.cmd" install npm -g
 }
 else {
--- src/CaC_ToolsInstall.sh
+++ src/CaC_ToolsInstall.sh
@@ -4,59 +4,12 @@ if [ -z "$(command -v brew)" ]; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-BrewInstalled=$(brew list)
-BrewCaskInstalled=$(brew cask list)
-
 brew tap caskroom/cask
-
-if echo $BrewInstalled | grep wget; then
-    echo 'wget already installed.'
-else
-    echo 'Installing wget.'
-    brew install wget
-fi
-
-if echo $BrewInstalled | grep git; then
-    echo 'git already installed.'
-else
-    echo 'Installing git.'
-    brew install git
-fi
-
-if echo $BrewInstalled | grep node; then
-    echo 'Node.Js already installed.'
-else
-    echo 'Installing Node.Js.'
-    brew install node
-fi
-
-if echo $BrewCaskInstalled | grep cakebrew; then
-    echo 'CakeBrew already installed.'
-else
-    echo 'Installing CakeBrew.'
-    brew cask install cakebrew
-fi
-
-if echo $BrewCaskInstalled | grep google-chrome; then
-    echo 'Google Chrome already installed.'
-else
-    echo 'Installing Google Chrome.'
-    brew cask install google-chrome
-fi
-
-if echo $BrewCaskInstalled | grep atom then
-    echo 'Atom already installed.'
-else
-    echo 'Installing Atom.'
-    brew cask install atom
-fi
-
-if echo $BrewCaskInstalled | grep gitkraken then
-    echo 'Git Kraken already installed.'
-else
-    echo 'Installing Git Kraken.'
-    brew cask install gitkraken
-fi
-
-echo 'Updating npm.'
+brew install wget
+brew install git
+brew install node
+brew cask install cakebrew
+brew cask install google-chrome
+brew cask install atom
+brew cask install gitkraken
 npm install npm -g
